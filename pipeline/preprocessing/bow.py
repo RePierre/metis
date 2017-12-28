e07b799fea124d4a116d7efff397cd6b211fd039
andrei <andrei.pruteanu@uberresearch.com>
2017-12-28 16:18:54 +0200

    2 more lines

--- pipeline/preprocessing/bow.py
+++ pipeline/preprocessing/bow.py
@@ -1,5 +1,5 @@
 from keras.preprocessing.text import Tokenizer
-
+import numpy as np
 
 def run():
     # define 5 documents
@@ -24,6 +24,12 @@ def run():
     # integer encode documents
     encoded_docs = t.texts_to_matrix(docs, mode='tfidf')
     print(encoded_docs)
+
+    for i in range(len(docs)):
+        for j in range(len(docs)):
+            if i != j:
+                prod = np.multiply(encoded_docs[i], encoded_docs[j])
+                print('Similarity score between text {} and text {} is: {}'.format(i+1, j+1, np.sum(prod)))
 
 
 if __name__ == "__main__":
