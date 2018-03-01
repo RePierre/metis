d80f2b62e00dcb21aa9ca23c6f809faea5226600
RePierre <petru.rebeja@gmail.com>
2018-03-01 06:44:51 +0200

    Refactored the model to use both texts in parallel.

--- pipeline/preprocessing/lstm.py
+++ pipeline/preprocessing/lstm.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Input
 from keras.optimizers import RMSprop
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import TensorBoard
@@ -8,6 +9,7 @@ from keras.callbacks import TensorBoard
 import numpy as np
 import spacy
 from argparse import ArgumentParser
+import csv
 
 INPUT_SIZE = 384
 
@@ -15,41 +17,53 @@ nlp = spacy.load('en')
 
 
 def read_text(file_path):
-    result = []
-    with open(file_path, 'rt', encoding='utf-8')as f:
-        for line in f.readlines():
-            parts = line.split('\t')
-            result.append(parts[0].strip())
-            result.append(parts[1].strip())
-    return result
+    with open(file_path, 'rt', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for line in reader:
+            yield (line[4], line[5], np.float32(line[3]))
 
 
-def vectorize(sentence):
-    result = [t.vector for t in nlp(sentence)]
-    result = np.asarray(result)
-    # reshape word vectors to [1 sample, time steps, num features]
-    result = np.reshape(result, (1, len(result), INPUT_SIZE))
-    return result
+def build_datasets(input):
+    T1 = []
+    T1 = []
+    Y = []
+    for sentence1, sentece2, score in enumerate(input):
+        T1.append([t.vector for t in nlp(sentece1)])
+        T2.append([t.vector for t in nlp(sentence2)])
+        Y.append(score)
 
+    T1 = pad_sequences(T1, maxlen=INPUT_SIZE, dtype='float32')
+    T1 = np.reshape(T1, (T1.shape[0], INPUT_SIZE, INPUT_SIZE))
 
-def build_datasets(text):
-    X = []
-    for sentence in text:
-        X.append([t.vector for t in nlp(sentence)])
-    Y = [np.mean(s, axis=0) for s in X]
-    X = pad_sequences(X, maxlen=INPUT_SIZE, dtype='float32')
-    X = np.reshape(X, (X.shape[0], INPUT_SIZE, INPUT_SIZE))
+    T2 = pad_sequences(T2, maxlen=INPUT_SIZE, dtype='float32')
+    T2 = np.reshape(T2, (T2.shape[0], INPUT_SIZE, INPUT_SIZE))
+    X = [T1, T2]
     Y = np.asarray(Y)
     return X, Y
 
 
 def run(args):
-    model = Sequential()
-    model.add(LSTM(INPUT_SIZE,
-                   batch_input_shape=(args.batch_size, INPUT_SIZE, INPUT_SIZE),
-                   stateful=args.stateful))
-    model.add(Dense(INPUT_SIZE, activation='linear'))
+    # Define the input nodes
+    text1 = Input(shape=(INPUT_SIZE, INPUT_SIZE), name='text1')
+    text2 = Input(shape=(INPUT_SIZE, INPUT_SIZE), name='text2')
 
+    # Create the shared LSTM node
+    shared_lstm = LSTM(INPUT_SIZE,
+                       batch_input_shape=(args.batch_size, INPUT_SIZE, INPUT_SIZE),
+                       stateful=args.stateful)
+
+    # Run inputs through shared layer
+    encoded1 = shared_lstm(text1)
+    encoded2 = shared_lstm(text2)
+
+    # Concatenate outputs
+    concatenated = keras.layers.concatenate([encoded1, encoded2])
+
+    # Create the output layer
+    # It should return a single number
+    output = Dense(1, activation='sigmoid')(concatenated)
+
+    model = Model(inputs=[text1, text2], outputs=output)
     optimizer = RMSprop(lr=args.learning_rate)
     model.compile(loss=args.loss,
                   optimizer=optimizer,
@@ -69,7 +83,10 @@ def run(args):
 def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument('--input-file',
-                        help='Path to the input file.',
+                        help="""Path to the input file.
+                        The file is expected to be a SemEval STS file
+                        with the following tab separated fields
+                        [genre filename year score sentence1 sentence2]""",
                         required=True)
     parser.add_argument('--epochs',
                         help='Number of epochs to use for training.',
