922b91dbe5879a7abcffe4d30bb5314ed79737ee
RePierre <petru.rebeja@gmail.com>
2018-04-05 06:55:36 +0300

    Started running multiple experiments.

---
+++ pipeline/preprocessing/experiments.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+from pandas import DataFrame
+import preprocessing.lstm as lstm
+import pandas
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('--experiments-file',
+                        help='Path to the file defining experiments.',
+                        default='experiments.csv',
+                        required=False)
+    return parser.parse_args()
+
+
+def run_experiments(args):
+    df = pandas.read_csv(args.experiments_file)
+    for row in df.itertuples():
+        print(row)
+        lstm.run(row)
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run_experiments(args)
