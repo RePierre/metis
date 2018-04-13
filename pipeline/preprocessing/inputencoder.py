c413731adf355434d505803714c7d798345e279a
RePierre <petru.rebeja@gmail.com>
2018-04-13 06:53:15 +0300

    Added method to read a sample.

--- pipeline/preprocessing/inputencoder.py
+++ pipeline/preprocessing/inputencoder.py
@@ -1,5 +1,6 @@
 import spacy
 import numpy as np
+import json
 from keras.preprocessing.sequence import pad_sequences
 
 INPUT_SIZE = 384
@@ -25,18 +26,66 @@ def encode_title(title):
 
 
 def encode_keywords(keywords):
-    return encode_title(keywords)
+    return encode_text(keywords)
 
 
 def encode_citations(num_citations):
     return np.full((INPUT_SIZE,), num_citations)
 
 
-def encode_affiliation(affiliation):
+def encode_affiliations(affiliations):
     aff = _load_affiliations()
-    affiliation = affiliation.lower()
-    affiliation_id = aff[affiliation] if affiliation in aff else -1
-    return np.full((INPUT_SIZE, 1), affiliation_id)
+    encoded_affiliations = np.full((INPUT_SIZE, 1), -1)
+    for index, affiliation in enumerate(affiliations):
+        affiliation = affiliation.lower()
+        affiliation_id = aff[affiliation] if affiliation in aff else -1
+        encoded_affiliations[index] = affiliation_id
+    return encoded_affiliations
+
+
+def read_sample(filename, text_time_steps=2000,
+                title_time_steps=100, keywords_time_steps=100):
+    with open(filename, 'rt') as f:
+        data = json.load(f)
+    text = pad_and_reshape(
+        encode_text(_build_text(data)),
+        text_time_steps)
+    affiliations = encode_affiliations(_build_affiliations(data))
+    citations = encode_citations(_load_citations(data))
+    keywords = pad_and_reshape(
+        encode_keywords(_build_keywords(data)),
+        keywords_time_steps)
+    title = pad_and_reshape(
+        encode_title(data['article_title']),
+        title_time_steps)
+    return title, affiliations, keywords, text, citations
+
+
+def _build_keywords(data):
+    keywords = []
+    for kw in data['keywords']:
+        keywords.append(kw['#text'])
+    return ' '.join(keywords)
+
+
+def _build_affiliations(data):
+    affiliations = []
+    for author in data['authors']:
+        for affiliation in author['affiliations']:
+            affiliations.append(affiliation)
+    return affiliations
+
+
+def _build_text(data):
+    text = []
+    for section in data['body']:
+        text.append(section['title'])
+        text.append(section['text'])
+    return '\n'.join(text)
+
+
+def _load_citations(data):
+    return 0
 
 
 def _load_affiliations():
