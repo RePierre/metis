fad58d27ea015b55bc2f92d0eb51042d16b950bc
RePierre <petru.rebeja@gmail.com>
2018-06-11 07:03:35 +0300

    Split the data into 2 separate chunks.

--- pipeline/preprocessing/dataset.py
+++ pipeline/preprocessing/dataset.py
@@ -13,18 +13,38 @@ def build_datasets(input_path, batch_size):
     txt1, txt2, aff1, aff2, cit1, cit2, kwd1, kwd2, ttl1, ttl2 = train_test_split(
         texts, affiliations, citations, keywords, titles,
         train_size=0.5, shuffle=False)
+
     txt1_train, txt1_test, \
         aff1_train, aff1_test, \
         cit1_train, cit1_test, \
         kwd1_train, kwd1_test, \
-        ttl1_train, ttl1_test, \
-        txt2_train, txt2_test, \
+        ttl1_train, ttl1_test = train_test_split(txt1, aff1, cit1, kwd1, ttl1)
+    txt1_train, txt1_test, \
+        aff1_train, aff1_test, \
+        cit1_train, cit1_test, \
+        kwd1_train, kwd1_test, \
+        ttl1_train, ttl1_test = _accomodate_batch_size(batch_size,
+                                                       txt1_train, txt1_test,
+                                                       aff1_train, aff1_test,
+                                                       cit1_train, cit1_test,
+                                                       kwd1_train, kwd1_test,
+                                                       ttl1_train, ttl1_test)
+    txt2_train, txt2_test, \
         aff2_train, aff2_test, \
         cit2_train, cit2_test, \
         kwd2_train, kwd2_test, \
-        ttl2_train, ttl2_test = _accomodate_batch_size(
-            batch_size,
-            train_test_split(txt1, aff1, cit1, kwd1, ttl1, txt2, aff2, cit2, kwd2, ttl2))
+        ttl2_train, ttl2_test = train_test_split(txt2, aff2, cit2, kwd2, ttl2)
+    txt2_train, txt2_test, \
+        aff2_train, aff2_test, \
+        cit2_train, cit2_test, \
+        kwd2_train, kwd2_test, \
+        ttl2_train, ttl2_test = _accomodate_batch_size(batch_size,
+                                                       txt2_train, txt2_test,
+                                                       aff2_train, aff2_test,
+                                                       cit2_train, cit2_test,
+                                                       kwd2_train, kwd2_test,
+                                                       ttl2_train, ttl2_test)
+
     input1_train = InputData(txt1_train, aff1_train, cit1_train, kwd1_train, ttl1_train)
     input2_train = InputData(txt2_train, aff2_train, cit2_train, kwd2_train, ttl2_train)
     input1_test = InputData(txt1_test, aff1_test, cit1_test, kwd1_test, ttl1_test)
