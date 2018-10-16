65a9939b422c5cc297a0f490ca44c6ad02c39b98
RePierre <petru.rebeja@gmail.com>
2018-10-16 23:35:10 +0300

    Try to run HDP with Countvectorizer from sk-learn.

--- analysis/hdp.py
+++ analysis/hdp.py
@@ -1,15 +1,40 @@
-from gensim.test.utils import common_corpus, common_dictionary
+import os
+from argparse import ArgumentParser
+from sklearn.feature_extraction.text import CountVectorizer
 from gensim.models import HdpModel
-hdp = HdpModel(common_corpus, common_dictionary)
-unseen_doc = [(1, 3.), (2, 4)]
-doc_hdp = hdp[unseen_doc]
-print(doc_hdp)
 
-topics = hdp.get_topics()
-num_topics, vocab_size = topics.shape
-print("Found {} topics.".format(num_topics))
-print("Printing top 20 topics")
-topic_info = hdp.print_topics(num_topics=20, num_words=20)
-for _, topic in enumerate(topic_info):
-    num, words = topic
-    print("Topic {}: {}".format(num, words))
+
+def get_file_names(input_path):
+    for root, dirs, files in os.walk(input_path):
+        for f in files:
+            yield os.path.join(root, f)
+
+
+def run(args):
+    vectorizer = CountVectorizer(input='filename')
+    X = vectorizer.fit_transform(get_file_names(args.input_path))
+    corpus = [[(idx, count) for idx, count in enumerate(doc) if count > 0]
+              for doc in X.toarray()]
+    dictionary = {idx: word
+                  for idx, word in enumerate(vectorizer.get_feature_names())}
+    hdp = HdpModel(corpus, dictionary)
+    topics = hdp.get_topics()
+    num_topics, vocab_size = topics.shape
+    print("Found {} topics.".format(num_topics))
+
+    print("Printing top 20 topics")
+    topic_info = hdp.print_topics(num_topics=20, num_words=20)
+    for _, topic in enumerate(topic_info):
+        num, words = topic
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('--input-path',
+                        help='The directory with parsed articles.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run(args)
