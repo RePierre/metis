4d43dff13be252a2ebfe3bb0a4a4215bafac4655
RePierre <petru.rebeja@gmail.com>
2018-10-21 23:17:28 +0300

    Added logging to file.

--- analysis/hdp.py
+++ analysis/hdp.py
@@ -1,5 +1,7 @@
 import os
 import json
+
+import logging
 from nltk.corpus import stopwords
 from argparse import ArgumentParser
 from sklearn.feature_extraction.text import CountVectorizer
@@ -7,18 +9,27 @@ from gensim.corpora import Dictionary
 from gensim.models import HdpModel
 
 
+logger = logging.getLogger(__name__)
+
+
 class Corpus:
     """A generator over the corpus files."""
 
-    def __init__(self, corpus_dir):
+    def __init__(self, corpus_dir, logger=None):
         self._corpus_dir = corpus_dir
         self._text_analyzer = Corpus._build_analyzer()
+        self._logger = logger
 
     def __iter__(self):
         for file_name in self._get_file_names():
+            self._log_info("Reading file {}.".format(file_name))
             try:
                 yield self._text_analyzer(get_file_text(file_name))
             except json.decoder.JSONDecodeError:
+                self._log_info("Reading failed.")
+                continue
+            except TypeError:
+                self._log_info("Reading failed.")
                 continue
 
     def _get_file_names(self):
@@ -29,6 +40,10 @@ class Corpus:
     def _build_analyzer():
         vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
         return vectorizer.build_analyzer()
+
+    def _log_info(self, message):
+        if self._logger:
+            self._logger.info(message)
 
 
 def get_file_text(file_name):
@@ -50,18 +65,22 @@ def tokenize_text(text):
 
 
 def run(args):
-    documents = Corpus(args.input_path)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)-15s %(levelname)s:%(name)s %(funcName)s: %(message)s',
+                        filename='hdp-analysis.log',
+                        filemode='w')
+    logger.info("Building dictionary from corpus...")
+    documents = Corpus(args.input_path, logger)
     dct = Dictionary.from_documents(documents)
+    logger.info("Finished building dictionary.")
+
+    logger.info("Running the HDP model.")
     corpus = [dct.doc2bow(doc) for doc in Corpus(args.input_path)]
     hdp = HdpModel(corpus, dct)
+    hdp.optimal_ordering()
     topics = hdp.get_topics()
     num_topics, vocab_size = topics.shape
-    print("Found {} topics.".format(num_topics))
-
-    print("Printing top 20 topics")
-    topic_info = hdp.print_topics(num_topics=20, num_words=20)
-    for _, topic in enumerate(topic_info):
-        num, words = topic
+    logger.info("Found {} topics. Vocabulary size: {}".format(num_topics, vocab_size))
 
 
 def parse_arguments():
