dc06d14d8ed974a1a2adc6da25b8503ebf2bf90f
RePierre <petru.rebeja@gmail.com>
2018-12-19 07:20:52 +0200

    Added debug logging.

--- analysis/fixedsizecorpus.py
+++ analysis/fixedsizecorpus.py
@@ -12,17 +12,26 @@ class FixedSizeCorpus:
         self._logger = logger
 
     def __iter__(self):
+        self._log_debug_message('Start iterating over corpus directory {}.'
+                                .format(self._corpus_dir))
         if not self._random_order:
+            self._log_debug_message('Random order set to false.')
             num_files = self._num_files
             for text in read_corpus(self._corpus_dir):
                 if num_files > 0:
                     num_files -= 1
                     yield text
         else:
+            self._log_debug_message('Random order set to true.')
+            self._log_debug_message('Reading all file names...')
             files = [f for f in get_file_names(self._corpus_dir)]
+            self._log_debug_message('Shuffling file names...')
             np.random.shuffle(files)
+            self._log_debug_message('Returning results.')
             for f in files[:self._num_files]:
                 yield get_file_text(f)
+        self._log_debug_message('Finished iterating over corpus directory {}.'
+                                .format(self._corpus_dir))
 
     def _log_debug_message(self, message):
         if self._logger:
